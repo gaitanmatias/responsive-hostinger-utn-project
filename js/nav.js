06807358b60ffbe0eaaef43bde08e18fe8dd1ed5
Gaitan Matias <gaitanmatiasg@gmail.com>
2025-05-07 06:32:23 -0300

    feat(nav): refactoriza estructura HTML, agrega estilos responsive y funcionalidad JS

---
+++ js/nav.js
@@ -0,0 +1,38 @@
+const navToggleBtn = document.getElementById('nav-toggle-btn')
+navToggleBtn.addEventListener('click', function() {
+  const navMenu = document.querySelector('.nav__links');
+  navMenu.classList.toggle('active');
+  if (navToggleBtn.classList.contains('fa-bars')) {
+    navToggleBtn.classList.replace('fa-bars', 'fa-xmark');
+  } else {
+    navToggleBtn.classList.replace('fa-xmark', 'fa-bars');
+  }
+});
+
+const mediaQuery = window.matchMedia('(max-width: 1100px)');
+const navArrows = document.querySelectorAll('.nav__arrow');
+function handleMediaQuery(e) {
+  if (e.matches) {
+    navArrows.forEach(arrow => {
+      arrow.classList.replace('fa-chevron-down', 'fa-chevron-right');
+    });
+  } else {
+    navArrows.forEach(arrow => {
+      arrow.classList.replace('fa-chevron-right', 'fa-chevron-down');
+    });
+  }
+}
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    nav.classList.add('nav--scrolled');
+  } else {
+    nav.classList.remove('nav--scrolled');
+  }
+});
+
+
+mediaQuery.addEventListener('change', handleMediaQuery);
+handleMediaQuery(mediaQuery);
